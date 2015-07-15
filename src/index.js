697563c42b8f1786c085cc5d6fa2e33c1c6d1f81
Patrick Mowrer <pmowrer@gmail.com>
2015-07-15 19:52:54 -0400

    Break apart into individual methods and expose them.
    Primarily allows for calling disableSpecs in other instances than
    just on detected spec failure.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import _ from 'lodash';
 
+let refs;
+
 // Jasmine doesn't yet have an option to fail fast. This "reporter" is a workaround for the time
 // being, making Jasmine essentially skip all tests after the first failure.
 // https://github.com/jasmine/jasmine/issues/414
 // https://github.com/juliemr/minijasminenode/issues/20
-export default function() {
+export function init() {
+  refs = getSpecReferences();
+
+  return {
+    specDone(result) {
+      if (result.status === 'failed') {
+        disableSpecs(refs);
+      }
+    }
+  };
+}
+
+/**
+ * Gather references to all jasmine specs and suites, through any (currently hacky) means possible.
+ *
+ * @return {Object} An object with `specs` and `suites` properties, arrays of respective types.
+ */
+export function getSpecReferences() {
   let specs = [];
   let suites = [];
 
@@ -23,20 +42,27 @@ export default function() {
     });
 
   return {
-    specDone(result) {
-      // Hacky workaround to facilitate "fail fast". Disable all specs (basically `xit`), then
-      // remove references to all before/after functions, else they'll still run. Disabling the
-      // suites themselves does not appear to have an effect.
-      if (result.status === 'failed') {
-        specs.forEach(spec => spec.disable());
-
-        suites.forEach(suite => {
-          suite.beforeFns = [];
-          suite.afterFns = [];
-          suite.beforeAllFns = [];
-          suite.afterAllFns = [];
-        });
-      }
-    }
+    specs,
+    suites
   };
+}
+
+/**
+ * Hacky workaround to facilitate "fail fast". Disable all specs (basically `xit`), then
+ * remove references to all before/after functions, else they'll still run. Disabling the
+ * suites themselves does not appear to have an effect.
+ */
+export function disableSpecs() {
+  if (!refs) {
+    throw new Error('jasmine-fail-fast: Must call init() before calling disableSpecs()!');
+  }
+
+  refs.specs.forEach(spec => spec.disable());
+
+  refs.suites.forEach(suite => {
+    suite.beforeFns = [];
+    suite.afterFns = [];
+    suite.beforeAllFns = [];
+    suite.afterAllFns = [];
+  });
 }
