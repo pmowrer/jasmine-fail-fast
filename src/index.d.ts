161abf5839ee94f37e3651ce605a548d129f0b7a
Erik Pukinskis <epukinskis@outlinerisk.com>
2019-10-22 13:34:09 -0700

    Proper module name in typings

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,4 @@
-declare module 'add-dashes' {
+declare module '@outlinerisk/jasmine-fail-hardcore' {
   export function shutItDown(): void
   export default function init(alreadyFailed: boolean, onFailure): () => void
 }
